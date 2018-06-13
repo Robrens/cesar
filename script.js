ce73d669a5664ac06b872a8eb0993331b7854cf3
Robrens <jbb.baudu@wanadoo.fr>
2018-06-13 11:24:20 +0200

    creation alphabet

---
+++ script.js
@@ -0,0 +1,15 @@
+var section = document.getElementsByTagName('div');
+// var table = document.createElement(table);
+// var row = document.createElement(tr);
+// var columns= document.createElement(td);
+//
+// section.appendChild(table);
+// table.appendChild(row);
+// row.appendChild(columns);
+
+var alphMatrix = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+var textA = document.createElement('imput');
+textA.type='text'
+console.log(textA);
+section.appendChild(textA);
