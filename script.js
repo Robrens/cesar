4e6c90694ea3778034562e6010d8cb9993353963
kamilacase <xakdxshooter@live.fr>
2018-06-13 12:06:48 +0200

    creation js valable

--- script.js
+++ script.js
@@ -8,8 +8,11 @@ var section = document.getElementsByTagName('div');
 // row.appendChild(columns);
 
 var alphMatrix = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-
-var textA = document.createElement('imput');
-textA.type='text'
-console.log(textA);
-section.appendChild(textA);
+var text = document.getElementById("cesar");
+var shift = 3;
+var content = text.innerText;
+console.log(content);
+for (var i < alphMatrix.length; i++) {
+    
+    
+}
